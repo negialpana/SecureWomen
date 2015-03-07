a00052158886bf9fafd62aa5991d5d8c52dcece3
Alpana Negi <AlpanaNegi@air-watch.com>
2015-03-07 22:46:08 +0530

    #noissue post to fb and twitter

--- SecureWomen/AlertViewController.m
+++ SecureWomen/AlertViewController.m
@@ -8,6 +8,10 @@
 
 #import "AlertViewController.h"
 #import "SettingsKeys.h"
+#import <Social/Social.h>
+
+#define TWEET_TEXT               @"Test Tweet"        //@"Please Help"
+#define POST_TEXT_TO_FB          @"Test Post"
 
 @interface AlertViewController ()
 
@@ -44,7 +48,54 @@
 - (IBAction)alertButtonPressed:(id)sender
 {
     NSLog(@"alertButtonPressed");
-
+    [self postToTwitter];
+    [self postToFB];
 }
+
+- (void)postToTwitter
+{
+    if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter])
+    {
+        SLComposeViewController *tweetSheet = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
+        [tweetSheet setInitialText:TWEET_TEXT];
+        [self presentViewController:tweetSheet animated:YES completion:nil];
+    }
+    else
+    {
+        UIAlertView *alertView = [[UIAlertView alloc]
+                                  initWithTitle:@"Sorry"
+                                  message:@"You can't send a tweet right now, make sure your device has an internet connection and you have at least one Twitter account setup"
+                                  delegate:self
+                                  cancelButtonTitle:@"OK"
+                                  otherButtonTitles:nil];
+        [alertView show];
+    }
+}
+
+- (void)postToFB
+{
+    if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeFacebook])
+    {
+        SLComposeViewController *fbSheet = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
+        [fbSheet setInitialText:POST_TEXT_TO_FB];
+        [self presentViewController:fbSheet animated:YES completion:nil];
+    }
+    else
+    {
+        UIAlertView *alertView = [[UIAlertView alloc]
+                                  initWithTitle:@"Sorry"
+                                  message:@"You can't send a post to fb right now, make sure your device has an internet connection and you have at least one fb account setup"
+                                  delegate:self
+                                  cancelButtonTitle:@"OK"
+                                  otherButtonTitles:nil];
+        [alertView show];
+    }
+}
+
+- (void)sendMessageForHelp
+{
+    
+}
+
 
 @end
