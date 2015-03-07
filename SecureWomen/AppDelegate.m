08e063dbc7f1db5cf4bc4583ce978ae04ccc5c2f
Supritha Nagesha <SNagesha@air-watch.com>
2015-03-07 18:16:34 +0530

    #noissue test commit

--- SecureWomen/AppDelegate.m
+++ SecureWomen/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
+    //Test
     [GMSServices provideAPIKey:GOOGLE_MAPS_APIKEY];
     // Override point for customization after application launch.
     return YES;
